cfa828ffa86e65263f7c9c5192d2fa958ae73ca1
高华峰 <gaohuafeng@58ganji.com>
2018-08-01 21:03:27 +0800

    优化发布

--- src/pages/publish/publish.js
+++ src/pages/publish/publish.js
@@ -12,6 +12,14 @@ export default {
       actions: [],
       menus: MenuConfig.publish_menus,
       menusIndex: 0,
+      title: '',
+      currentDate: '',
+      showDate: false,
+      formateDate: '',
+      tags: '',
+      area: '',
+      price: '',
+      des: ''
     }
   },
   methods: {
@@ -25,12 +33,46 @@ export default {
     },
     selectedCallback(e) {
       this.menus[this.menusIndex].selected = this.actions.indexOf(e)
-      console.log(this.menus)
       this.showAreaSelect = false
     },
     submit() {
       // api
-      Toast.success('提交成功');
+      let result = this.checkForm()
+      if (!result) {
+        this.$toast({
+          message: '有必填信息未填写',
+          duration: 1500
+        })
+      }
+    },
+    confirmDate (date) {
+      let d = new Date(date)
+
+      let year = d.getFullYear()
+      let month = d.getMonth() + 1
+      month = month > 9 ? month : '0' + month
+      let day = d.getDate()
+      day = day > 9 ? day : '0' + day
+
+      this.formateDate = `${year}-${month}-${day}`
+      this.showDate = false
+    },
+    checkForm () {
+      let menus = this.menus
+      let selected = []
+      let result = false
+      menus.forEach((item) => {
+        result = item.selected === -1 ? false : true
+        selected.push(item.selected)
+      })
+      if (!result) {
+        return result
+      }
+      result = this.title && this.currentDate &&
+               this.tags && this.area && this.price &&
+               this.des
+
+      return result
     }
   },
   components: {}
--- src/pages/publish/publish.vue
+++ src/pages/publish/publish.vue
@@ -1,7 +1,22 @@
 <template>
-	<div>
+	<div class="tabbar-padding">
   		<van-cell-group>
-		  <van-cell v-for="(menu, index) in menus" :key="index" :title="menu.title" @click="showArea(index)" :value="menu.list[menu.selected]" />
+		  <van-field v-model="title" placeholder="请输入房源标题" label="标题" input-align="right" required />
+		  <van-field v-model="price" placeholder="请输入房源价格" label="价格" input-align="right" type="number" required/>
+		  <van-field v-model="tags" placeholder="请输入房源标签，以逗号分隔" label="标签" input-align="right" required />
+		  <van-field v-model="area" placeholder="请输入房源面积，单位为平米" label="面积" input-align="right" required />
+		  <van-cell title="年代" :value="formateDate" @click="showDate = !showDate" required />
+		  <van-popup v-model="showDate" position="bottom">
+		  	<van-datetime-picker
+		  	  v-model="currentDate"
+		  	  @confirm="confirmDate"
+		  	  @cancel="showDate = false"
+		  	  :min-date="new Date('1980')"
+		  	  :max-date="new Date()"
+		  	  type="date"/>
+		  </van-popup>
+		  <van-cell v-for="(menu, index) in menus" :key="index" :title="menu.title" @click="showArea(index)" :value="menu.list[menu.selected]" required/>
+		  <van-field v-model="des" placeholder="请输入房源详细描述" label="详细描述" type="textarea" required />
 		</van-cell-group>
 		 <div class="btns">
 			<van-button @click="submit" size="large">提交</van-button>
